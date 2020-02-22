9af7b4ebe26b939ec145c0593415dc0b36fdebc0
Michał Ossowski <michalo2882@gmail.com>
2020-02-22 21:05:22 +0100

    display items

--- src/Items.js
+++ src/Items.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 class Items extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  async componentDidMount() {
+    let resp = await fetch("/api/v1/items");
+    if (resp.ok) {
+      let data = await resp.json();
+      this.setState({items: data});
+    }
+  }
+
   render() {
-    return <p>Items</p>;
+    return (
+      <ul>
+        {this.state.items.map((item, idx) =>
+          <li key={idx}>{item.name}</li>)}
+      </ul>
+    )
   }
 }
 
