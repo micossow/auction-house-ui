b2a1f6b967b85a98a014942d3c66708460d83e31
Michał Ossowski <michalo2882@gmail.com>
2020-02-22 20:48:06 +0100

    initialize template with react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuth: false,
+      user: null
+    }
+  }
+
+  async componentDidMount() {
+    let resp = await fetch("/api/v1/dashboard");
+    if (resp.ok) {
+      let data = await resp.json();
+      this.setState({isAuth: true, user: data.user});
+    } else if (resp.status === 403) {
+      this.setState({isAuth: false});
+    }
+  }
+
+  render() {
+    if (this.state.isAuth) {
+      return (
+        <Router>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"/>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <NavLink exact to="/" className="nav-link" activeClassName="active">Dashboard</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/items" className="nav-link" activeClassName="active">Items</NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <div className="container-fluid">
+            <Switch>
+              <Route path="/items">
+                <p>...</p>
+              </Route>
+              <Route path="/">
+                <p>...</p>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      );
+    } else {
+      return (<p>Login issue</p>);
+    }
+  }
 }
 
 export default App;
