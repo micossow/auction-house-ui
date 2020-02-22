62c112f12c259d0d23a61077390454b143453e88
Michał Ossowski <michalo2882@gmail.com>
2020-02-22 21:22:18 +0100

    display dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,8 +1,67 @@
 import React from 'react';
 
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      wallet: {coins: 0},
+      buyListings: [],
+      sellListings: [],
+      inventory: [],
+    }
+  }
+
+  async componentDidMount() {
+    let resp = await fetch("/api/v1/dashboard");
+    if (resp.ok) {
+      let data = await resp.json();
+      this.setState({
+        user: data.user,
+        wallet: data.wallet,
+        buyListings: data.buyListings,
+        sellListings: data.sellListings,
+        inventory: data.inventory,
+      });
+    }
+  }
+
   render() {
-    return <p>Dashboard</p>;
+    return (
+      <React.Fragment>
+        <div className="row">
+          <div className="col">
+            <h3>Welcome <strong>{this.state.user}</strong></h3>
+            <p>You have <strong>{this.state.wallet.coins}</strong> coins</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <h3>Your buy listings</h3>
+            <ul>
+              {this.state.buyListings.map((listing, idx) =>
+                <li key={idx}>{listing.description}</li>)}
+            </ul>
+          </div>
+          <div className="col-3">
+            <h3>Your sell listings</h3>
+            <ul>
+              {this.state.sellListings.map((listing, idx) =>
+                <li key={idx}>{listing.description}</li>)}
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h3>Inventory</h3>
+            <ul>
+              {this.state.inventory.map((inventoryItem, idx) =>
+                <li key={idx}>{inventoryItem.description}</li>)}
+            </ul>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
