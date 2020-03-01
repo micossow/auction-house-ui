84fe8dfcf47ddbb18edbccd0a226b8887129e1b7
Michał Ossowski <michalo2882@gmail.com>
2020-03-01 17:05:47 +0100

    show items as cards

--- src/Items.js
+++ src/Items.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+
+function Item(props) {
+  return <div className="col-sm-12 col-md-6 col-lg-4 col-xl-2 mb-3">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{props.data.name}</h5>
+        <NavLink to={`/item/${props.data.id}/buy`} className="card-link">Buy</NavLink>
+      </div>
+    </div>
+  </div>
+}
 
 class Items extends React.Component {
   constructor(props) {
@@ -18,10 +30,10 @@ class Items extends React.Component {
 
   render() {
     return (
-      <ul>
+      <div className="row">
         {this.state.items.map((item, idx) =>
-          <li key={idx}>{item.name}</li>)}
-      </ul>
+          <Item key={idx} data={item}/>)}
+      </div>
     )
   }
 }
