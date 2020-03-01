d3a5c3033a64673bbaac1bf133f7e53869d75a86
Michał Ossowski <michalo2882@gmail.com>
2020-03-01 18:58:09 +0100

    add current listings to sell form

--- src/InventoryItemSell.js
+++ src/InventoryItemSell.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {useHistory, useParams} from "react-router-dom";
 import fetchWithAuth from "./Utils";
+import ItemListings from "./ItemListings";
 
 class InventoryItemSellForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
       inventoryItem: {
         item: {name: ''},
         count: 0
@@ -21,6 +23,7 @@ class InventoryItemSellForm extends React.Component {
       let data = await resp.json();
       this.setState({inventoryItem: data});
     }
+    this.setState({loading: false})
   }
 
   update(field, e) {
@@ -45,30 +48,33 @@ class InventoryItemSellForm extends React.Component {
   }
 
   render() {
-    return (
-        <div className="row">
-          <div className="col">
-            {this.state.errors ?
-                <div className="alert alert-danger" role="alert">{this.state.errors}</div> : <span/>}
-            <p>You have {this.state.inventoryItem.count} of {this.state.inventoryItem.item.name}</p>
-            <form onSubmit={(e) => this.sell(e)}>
-              <div className="form-group">
-                <label htmlFor="count">Count</label>
-                <input type="number" placeholder="Count" id="count" className="form-control" value={this.state.count}
-                       onChange={(e) => this.update('count', e)}/>
-              </div>
-              <div className="form-group">
-                <label htmlFor="count">Price</label>
-                <input type="number" placeholder="Price" id="price" className="form-control" value={this.state.price}
-                       onChange={(e) => this.update('price', e)}/>
-              </div>
-              <input type="submit" className="btn btn-primary"
-                     disabled={!(this.state.count && this.state.price)}
-                     value="Sell"/>
-            </form>
-          </div>
+    return this.state.loading ? <p>Loading...</p> : <>
+      <div className="row mb-3">
+        <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+          {this.state.errors ?
+              <div className="alert alert-danger" role="alert">{this.state.errors}</div> : <span/>}
+          <p>You have {this.state.inventoryItem.count} of {this.state.inventoryItem.item.name}</p>
+          <form onSubmit={(e) => this.sell(e)}>
+            <div className="form-group">
+              <label htmlFor="count">Count</label>
+              <input type="number" placeholder="Count" id="count" className="form-control" value={this.state.count}
+                     onChange={(e) => this.update('count', e)}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="count">Price</label>
+              <input type="number" placeholder="Price" id="price" className="form-control" value={this.state.price}
+                     onChange={(e) => this.update('price', e)}/>
+            </div>
+            <input type="submit" className="btn btn-primary"
+                   disabled={!(this.state.count && this.state.price)}
+                   value="Sell"/>
+          </form>
         </div>
-    )
+      </div>
+      <div className="row">
+        <ItemListings itemId={this.state.inventoryItem.item.id}/>
+      </div>
+    </>;
   }
 }
 
